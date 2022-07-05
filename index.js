e3e2149bd249e0fb34a578efa74e7214990c0743
Maria Coitinho <brasildu@gmail.com>
2022-07-05 17:29:06 -0500

    commit tree eda4c6e1742cfd6c345fb84010f670391267b589

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "Maria Eduarda",
+    streetAddress: "405 N Wabash Ave",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeCopy = {...employee};
+    employeeCopy[key] = value;
+    return employeeCopy;
+}   
+
+// console.log(updateEmployeeWithKeyAndValue(employee, "streetAddress", "123 N Duda St"))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Vladimir"));
+// console.log(employee);
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy = {...employee};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+// console.log(deleteFromEmployeeByKey(employee, "name"));
+// console.log(employee);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;    
+}
+
+// console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"));
+// console.log(employee);
